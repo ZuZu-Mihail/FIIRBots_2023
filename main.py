947e6dc5c2326650d68d105f5974024e9a9be900
ZuZu-Mihail <mihailhanga@gmail.com>
2023-11-04 00:54:22 +0200

    facut pentru maze 2

--- main.py
+++ main.py
@@ -37,9 +37,41 @@ GPIO.setup(in4,GPIO.OUT)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 
+pi1_pwm = GPIO.PWM(in1, 1000)
+pi2_pwm = GPIO.PWM(in2, 1000)
+pi3_pwm = GPIO.PWM(in3, 1000)
+pi4_pwm = GPIO.PWM(in4, 1000)
+
+pi1_pwm.start(0)
+pi2_pwm.start(0)
+pi3_pwm.start(0)
+pi4_pwm.start(0)
+
+speed = 0
+
+def PWM_Setup(x):
+    global speed
+    if x > 0:
+        pi1_pwm.ChangeDutyCycle(speed)
+        pi2_pwm.ChangeDutyCycle(0)
+        pi3_pwm.ChangeDutyCycle(speed)
+        pi4_pwm.ChangeDutyCycle(0)
+    elif x < 0:
+        pi1_pwm.ChangeDutyCycle(0)
+        pi2_pwm.ChangeDutyCycle(speed)
+        pi3_pwm.ChangeDutyCycle(0)
+        pi4_pwm.ChangeDutyCycle(speed)
+
+    else:
+        pi1_pwm.ChangeDutyCycle(0)
+        pi2_pwm.ChangeDutyCycle(0)
+        pi3_pwm.ChangeDutyCycle(0)
+        pi4_pwm.ChangeDutyCycle(0)
+
+
 
 # p=GPIO.PWM(en,1000)
-p.start(25)
+# p.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -48,28 +80,33 @@ print("\n")
 
 
 def forward():
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
+    # GPIO.output(in1,GPIO.HIGH)
+    # GPIO.output(in2,GPIO.LOW)
 
-    GPIO.output(in3,GPIO.HIGH) # other motor
-    GPIO.output(in4,GPIO.LOW)
+    # GPIO.output(in3,GPIO.HIGH) # other motor
+    # GPIO.output(in4,GPIO.LOW)
+    PWM_Setup(1)
 
 def backward():
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.HIGH)
+    # GPIO.output(in1,GPIO.LOW)
+    # GPIO.output(in2,GPIO.HIGH)
 
-    GPIO.output(in3,GPIO.LOW) # other motor
-    GPIO.output(in4,GPIO.HIGH)
+    # GPIO.output(in3,GPIO.LOW) # other motor
+    # GPIO.output(in4,GPIO.HIGH)
+    PWM_Setup(-1)
 
 def stop():
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.LOW)
+    # GPIO.output(in1,GPIO.LOW)
+    # GPIO.output(in2,GPIO.LOW)
 
-    GPIO.output(in3,GPIO.LOW) # other motor
-    GPIO.output(in4,GPIO.LOW)
+    # GPIO.output(in3,GPIO.LOW) # other motor
+    # GPIO.output(in4,GPIO.LOW)
+    PWM_Setup(0)
 
 def speed(x):
-    p.ChangeDutyCycle(int(x)*10)
+    # p.ChangeDutyCycle(int(x)*10)
+    global speed
+    speed = int(x) * 10
 
 
 
