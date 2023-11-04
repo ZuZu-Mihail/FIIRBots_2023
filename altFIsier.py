1cd010ae1746c6f0e29d87965d500581018ad0ed
EnderReal <66971229+EnderReal@users.noreply.github.com>
2023-11-04 05:32:19 +0200

    distance

--- altFIsier.py
+++ altFIsier.py
@@ -1,5 +1,6 @@
 from mpu6050 import MPU6050
 from time import sleep
+from vl53l0x import VL53L0X, Vl53l0xAccuracyMode
 
 mpu = MPU6050()
 
@@ -7,6 +8,16 @@ mpu.Initialize()
 sleep(0.1)
 mpu.Calibrate()
 
+tof = VL53L0X()
+
+tof.open()
+
+tof.start_ranging(Vl53l0xAccuracyMode.GOOD)
+
 while True:
-    mpu.read()
-    print(mpu._angZ)
+    distance = tof.get_distance()
+    print(distance)
+
+# while True:
+#     mpu.read()
+#     print(mpu._angZ)
