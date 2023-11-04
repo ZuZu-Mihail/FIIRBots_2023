089910abfbe4ce501f41bab0a580e895db000ca3
EnderReal <66971229+EnderReal@users.noreply.github.com>
2023-11-04 06:38:16 +0200

    tof 8

--- altFIsier.py
+++ altFIsier.py
@@ -1,12 +1,21 @@
 from mpu6050 import MPU6050
 from time import sleep
-# from vl53l0x import VL53L0X, Vl53l0xAccuracyMode
+import VL53L0X
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X()
+
+# Start ranging
+tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+
+print(tof.get_distance())
 
 mpu = MPU6050()
 
 mpu.Initialize()
 sleep(0.1)
 mpu.Calibrate()
+
 
 
 
--- vl53l0x.py
+++ vl53l0x.py
@@ -1,190 +1,117 @@
-#!/usr/bin/env python
-#
-#
-from __future__ import print_function
-import _rpi_vl53l0x as vl
+#!/usr/bin/python
 
-VERSION_REQUIRED_MAJOR = 1
-VERSION_REQUIRED_MINOR = 0
-VERSION_REQUIRED_BUILD = 2
+# MIT License
+#
+# Copyright (c) 2017 John Bryan Moore
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-_GPIO_NEW_SAMPLE_READY = 0x04
+import time
+from ctypes import *
+import smbus
+
+VL53L0X_GOOD_ACCURACY_MODE      = 0   # Good Accuracy mode
+VL53L0X_BETTER_ACCURACY_MODE    = 1   # Better Accuracy mode
+VL53L0X_BEST_ACCURACY_MODE      = 2   # Best Accuracy mode
+VL53L0X_LONG_RANGE_MODE         = 3   # Longe Range mode
+VL53L0X_HIGH_SPEED_MODE         = 4   # High Speed mode
+
+i2cbus = smbus.SMBus(1)
+
+# i2c bus read callback
+def i2c_read(address, reg, data_p, length):
+    ret_val = 0;
+    result = []
+ 
+    try:
+        result = i2cbus.read_i2c_block_data(address, reg, length)
+    except IOError:
+        ret_val = -1; 
+
+    if (ret_val == 0):
+        for index in range(length):
+            data_p[index] = result[index]
+
+    return ret_val
+
+# i2c bus write callback
+def i2c_write(address, reg, data_p, length):
+    ret_val = 0;
+    data = []
+
+    for index in range(length):
+        data.append(data_p[index])
+    try:
+        i2cbus.write_i2c_block_data(address, reg, data)
+    except IOError:
+        ret_val = -1; 
+
+    return ret_val
+
+# Load VL53L0X shared lib 
+tof_lib = CDLL("../bin/vl53l0x_python.so")
+
+# Create read function pointer
+READFUNC = CFUNCTYPE(c_int, c_ubyte, c_ubyte, POINTER(c_ubyte), c_ubyte)
+read_func = READFUNC(i2c_read)
+
+# Create write function pointer
+WRITEFUNC = CFUNCTYPE(c_int, c_ubyte, c_ubyte, POINTER(c_ubyte), c_ubyte)
+write_func = WRITEFUNC(i2c_write)
+
+# pass i2c read and write function pointers to VL53L0X library
+tof_lib.VL53L0X_set_i2c(read_func, write_func)
 
 class VL53L0X(object):
-    def __init__(self, address = 0x29, bus = 0x1):
-        self.dev_addr = address
-        self.dev = vl.new_VL53L0X_Dev_t()
-        vl.VL53L0X_Dev_t_I2cDevAddr_set(self.dev, self.dev_addr)
+    """VL53L0X ToF."""
 
-        fd = vl.VL53L0X_i2c_init("/dev/i2c-%d" % bus, self.dev_addr)
-        if fd < 0:
-            self._err = vl.VL53L0X_ERROR_CONTROL_INTERFACE
-            print("Failed to initialize i2c interface")
-        else:
-            self._err = vl.VL53L0X_ERROR_NONE
-            vl.VL53L0X_Dev_t_fd_set(self.dev, fd)
+    object_number = 0
 
-    def __del__(self):
-        st = vl.VL53L0X_StopMeasurement(self.dev)
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = self.__wait_stoped()
-        if st == vl.VL53L0X_ERROR_NONE:
-            vl.VL53L0X_ClearInterruptMask(self.dev, _GPIO_NEW_SAMPLE_READY)
-        vl.delete_VL53L0X_Dev_t(self.dev)
-        vl.VL53L0X_i2c_close()
-        self._err = st
+    def __init__(self, address=0x29, TCA9548A_Num=255, TCA9548A_Addr=0, **kwargs):
+        """Initialize the VL53L0X ToF Sensor from ST"""
+        self.device_address = address
+        self.TCA9548A_Device = TCA9548A_Num
+        self.TCA9548A_Address = TCA9548A_Addr
+        self.my_object_number = VL53L0X.object_number
+        VL53L0X.object_number += 1
 
-    def get_libver(self):
-        version = None
-        ver = vl.new_VL53L0X_Version_t()
-        st = vl.VL53L0X_GetVersion(ver)
-        if st == vl.VL53L0X_ERROR_NONE:
-            major = vl.VL53L0X_Version_t_major_get(ver)
-            minor = vl.VL53L0X_Version_t_minor_get(ver)
-            build = vl.VL53L0X_Version_t_build_get(ver)
-            revision = vl.VL53L0X_Version_t_revision_get(ver)
-            version = {
-                'major'   : major,
-                'minor'   : minor,
-                'build'   : build,
-                'revision': revision,
-            }
-            '''
-            if major != VERSION_REQUIRED_MAJOR or \
-               minor != VERSION_REQUIRED_MINOR or build != VERSION_REQUIRED_BUILD:
-                print("VL53L0X API Version Error: ")
-                print("  Your firmware has %d.%d.%d (revision %d)." % (major, minor, build, revision))
-                print("  This example requires %d.%d.%d.\n" % \
-                    (VERSION_REQUIRED_MAJOR, VERSION_REQUIRED_MINOR, VERSION_REQUIRED_BUILD))
-            '''
-        vl.delete_VL53L0X_Version_t(ver)
-        self._err = st
-        return version
-
-    def get_devver(self):
-        version = None
-        devinfo = vl.new_VL53L0X_DeviceInfo_t()
-        st = vl.VL53L0X_GetDeviceInfo(self.dev, devinfo)
-        if st == vl.VL53L0X_ERROR_NONE:
-            version = {
-                'type' : vl.VL53L0X_DeviceInfo_t_Type_get(devinfo),
-                'name' : vl.VL53L0X_DeviceInfo_t_Name_get(devinfo),
-                'id'   : vl.VL53L0X_DeviceInfo_t_ProductId_get(devinfo),
-                'major': vl.VL53L0X_DeviceInfo_t_ProductRevisionMajor_get(devinfo),
-                'minor': vl.VL53L0X_DeviceInfo_t_ProductRevisionMinor_get(devinfo),
-            }
-        vl.delete_VL53L0X_DeviceInfo_t(devinfo)
-        self._err = st
-        return version
-
-    def begin(self):
-        refSpandCount   = vl.new_uint32_t(1)
-        isApertureSpads = vl.new_uint8_t(1)
-        VhvSettings     = vl.new_uint8_t(1)
-        PhaseCal        = vl.new_uint8_t(1)
-
-        st = vl.VL53L0X_DataInit(self.dev)
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = vl.VL53L0X_StaticInit(self.dev)
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = vl.VL53L0X_PerformRefCalibration(self.dev, VhvSettings, PhaseCal)
-            '''
-            print("VhvSettings = {} PhaseCal = {}".format(
-                vl.uint8_t___getitem__(VhvSettings,0),
-                vl.uint8_t___getitem__(PhaseCal,0)))
-            '''
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = vl.VL53L0X_PerformRefSpadManagement(self.dev, refSpandCount, isApertureSpads)
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = vl.VL53L0X_SetDeviceMode(self.dev, vl.VL53L0X_DEVICEMODE_CONTINUOUS_RANGING)
-        if st == vl.VL53L0X_ERROR_NONE:
-            st = vl.VL53L0X_StartMeasurement(self.dev)
-
-        vl.delete_uint8_t(PhaseCal)
-        vl.delete_uint8_t(VhvSettings)
-        vl.delete_uint8_t(isApertureSpads)
-        vl.delete_uint32_t(refSpandCount)
-        self._err = st
-        return st
-
-    def __wait_stoped(self):
-        st = vl.VL53L0X_ERROR_NONE
-        stop_completed = vl.new_uint32_t(1)
-
-        loop_nr = 0
-        while True:
-            st = vl.VL53L0X_GetStopCompletedStatus(self.dev, stop_completed)
-            if vl.uint32_t___getitem__(stop_completed, 0) == 0 or \
-              st != vl.VL53L0X_ERROR_NONE:
-                break
-            loop_nr += 1
-            vl.VL53L0X_PollingDelay(self.dev)
-            if loop_nr >= vl.VL53L0X_DEFAULT_MAX_LOOP:
-                st = vl.VL53L0X_ERROR_TIME_OUT
-                break
-        vl.delete_uint32_t(stop_completed)
-        return st
-
-    def is_ready(self):
-        data_ready = vl.new_uint8_t(1)
-        ret_val = False
-
-        st = vl.VL53L0X_GetMeasurementDataReady(self.dev, data_ready)
-        if st != vl.VL53L0X_ERROR_NONE:
-            ret_val = None
-        elif vl.uint8_t___getitem__(data_ready,0) == 0x01:
-            ret_val = True
-
-        vl.delete_uint8_t(data_ready)
-        self._err = st
-        return ret_val
-
-    def wait_ready(self):
-        '''
-        return None  -- Error status
-               False -- Timeout
-               True  -- Ready
-        '''
-        loop_nr = 0
-        while True:
-            r = self.is_ready()
-            if r is None or r:
-                return r
-            loop_nr += 1
-            vl.VL53L0X_PollingDelay(self.dev)
-            if loop_nr >= vl.VL53L0X_DEFAULT_MAX_LOOP:
-                break
-        return False
+    def start_ranging(self, mode = VL53L0X_GOOD_ACCURACY_MODE):
+        """Start VL53L0X ToF Sensor Ranging"""
+        tof_lib.startRanging(self.my_object_number, mode, self.device_address, self.TCA9548A_Device, self.TCA9548A_Address)
+        
+    def stop_ranging(self):
+        """Stop VL53L0X ToF Sensor Ranging"""
+        tof_lib.stopRanging(self.my_object_number)
 
     def get_distance(self):
-        measured = vl.new_VL53L0X_RangingMeasurementData_t()
-        st = vl.VL53L0X_GetRangingMeasurementData(self.dev, measured)
-        dis = vl.VL53L0X_RangingMeasurementData_t_RangeMilliMeter_get(measured)
-        vl.VL53L0X_ClearInterruptMask(self.dev, _GPIO_NEW_SAMPLE_READY)
-        vl.delete_VL53L0X_RangingMeasurementData_t(measured)
-        self._err = st
-        return dis
+        """Get distance from VL53L0X ToF Sensor"""
+        return tof_lib.getDistance(self.my_object_number)
 
-def main():
-    print("VL53L0X Ranging example")
-
-    vl53 = VL53L0X()
-    vl53.begin()
-
-    '''
-    version = vl53.get_devver()
-    print("VL53L0X_GetDeviceInfo:")
-    print("Device Type   : %s" % version["type"])
-    print("Device Name   : %s" % version["name"])
-    print("Device ID     : %s" % version["id"])
-    print("RevisionMajor : %d" % version["major"])
-    print("RevisionMinor : %d" % version["minor"])
-    '''
-
-    st = vl53.wait_ready()
-    if st:
-        print("Distance = {} mm".format(vl53.get_distance()))
-
-
-if __name__ == '__main__':
-    main()
+    # This function included to show how to access the ST library directly
+    # from python instead of through the simplified interface
+    def get_timing(self):
+        Dev = POINTER(c_void_p)
+        Dev = tof_lib.getDev(self.my_object_number)
+        budget = c_uint(0)
+        budget_p = pointer(budget)
+        Status =  tof_lib.VL53L0X_GetMeasurementTimingBudgetMicroSeconds(Dev, budget_p)
+        if (Status == 0):
+            return (budget.value + 1000)
+        else:
+            return 0
